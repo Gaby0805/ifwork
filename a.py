ba57bc407318b34a007cf2da4fa49d55fd45a21d
Gabriel Amorim <gabbiebrabo@gmail.com>
2024-09-30 21:46:37 -0400

    bread bread

--- a.py
+++ a.py
@@ -1,7 +1,21 @@
-user_dict = {"gabriel": {"gmail": "g@gmail.com", "senha": "123"},"calebe": {"gmail": "j@gmail.com", "senha": "1232"}}
+# Dicionário para armazenar os dados dos usuários
+user_dict = {}
 
-# Acessando o valor da senha
-senha = user_dict["calebe"]["gmail"] 
-print(senha)
-senha = user_dict["calebe"]["gmail"] = "sexo1"
-print(senha)
+# Função para adicionar usuários
+def adicionar_usuario(user_dict, nome, email, senha):
+    # Verificar se o usuário já existe 
+    if nome in user_dict:
+        print(f"Usuário {nome} já existe!")
+        return
+    
+    # Adicionar o novo usuário ao dicionário
+    user_dict[nome] = {"gmail": email, "senha": senha}
+    print(f"Usuário {nome} adicionado com sucesso.")
+
+# Exemplo de uso da função
+adicionar_usuario(user_dict, "gabriel", "g@gmail.com", "123")
+adicionar_usuario(user_dict, "joao", "joao@gmail.com", "456")
+adicionar_usuario(user_dict, "maria", "maria@gmail.com", "789")
+
+# Exibindo o dicionário atualizado
+print(user_dict)
--- create.py
+++ create.py
@@ -1,20 +1,35 @@
 import flet as ft
 from flet_route import Params, Basket
 
-user_dict = {"gabriel":{"gmail: g@gmail.com ","senha: 123"}}
+user_dict = {}
 
-def create_user(page: ft.Page, params:Params, basket: Basket):
+def create_user(page:ft.Page, params:Params,basket:Basket ):
     
+        
+            
     def send(e):
+        nome_v = nome.value
         gmail_v = gmail.value     
         senha_v = senha.value
-
-    
-    
+        add_user(nome_v,gmail_v,senha_v)
+        
+    def add_user(nome,email,senha):
+        user_dict[nome] = {"email": email, "senha": senha}
+        print(user_dict)
+            
+            
+    nome = ft.TextField(label="nome")
     gmail = ft.TextField(label='gmail' )
     senha = ft.TextField(label='senha' )
     send_button = ft.ElevatedButton("criar",on_click=send)
-    
-    
+    excluir_item = ft.ElevatedButton("excluir", on_click=lambda e: page.go("/excluir"))
+    ft.View(
+            "/criacao",controls=[
+            nome,
+            gmail,
+            senha,
+            send_button,
+            excluir_item ]
+    )
     
     
